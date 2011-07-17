6217ee228b669205d27b2de98de4079ab1d58e27
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-17 19:47:39 +0800

    add: post controller and pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
 end
