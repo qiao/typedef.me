93d5962a0176bd925bc26814df3c9bb15724d9c3
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-17 19:20:34 +0800

    add: Commentable model details

--- app/models/commentable.rb
+++ app/models/commentable.rb
@@ -1,2 +1,11 @@
 class Commentable < ActiveRecord::Base
+
+  has_many :comments, :dependent => :destroy
+  validates :title,   :presence => true
+  validates :content, :presence => true
+  validates :slug,    :presence => true
+  
+  scope :published, where(:published => true)
+  scope :draft, where(:published => false)
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TypedefMe::Application.routes.draw do
+  get "pages/home"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
