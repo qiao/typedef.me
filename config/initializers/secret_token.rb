cfa94b43528d5b14fcc0f329b69ca52bff201270
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-17 19:01:27 +0800

    hello, world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TypedefMe::Application.config.secret_token = '6d2061a93698ea3aa753533aa283c55918e39505e924092a5e36f2a45211a52d5f9aefa20297bf51669647726a476a48a5721d5e604d69914c7c5ef4999ffac4'
