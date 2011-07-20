8baf0edea5087b2e49bde9290d73ca5707e02252
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-21 02:04:59 +0800

    add: comments display toggle for homepage and individual post page

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,9 +1,9 @@
-function show_comments_of_post_id(id) {
-  $("#post_" + id + "_comments").slideDown();
+function show_comments_of_commentable_id(id) {
+  $("#commentable_" + id + "_comments").slideDown();
   $("#comments_show_button_" + id).fadeOut();
 }
 
-function hide_comments_of_post_id(id) {
-  $("#post_" + id + "_comments").slideUp();
+function hide_comments_of_commentable_id(id) {
+  $("#commentable_" + id + "_comments").slideUp();
   $("#comments_show_button_" + id).fadeIn();
 }
