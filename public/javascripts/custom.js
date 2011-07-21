d1e5ff70302a2e5bb53ed03e213a2a24a0f62997
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-21 13:53:19 +0800

    fix: slidedown for comments

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,9 +1,9 @@
 function show_comments_of_commentable_id(id) {
-  $("#commentable_" + id + "_comments").slideDown();
+  $("#comments_list_" + id).slideDown();
   $("#comments_show_button_" + id).fadeOut();
 }
 
 function hide_comments_of_commentable_id(id) {
-  $("#commentable_" + id + "_comments").slideUp();
+  $("#comments_list_" + id).slideUp();
   $("#comments_show_button_" + id).fadeIn();
 }
