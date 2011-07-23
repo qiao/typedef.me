1d93dfbfb9881b37c608d00e7e7d383391e1f9a3
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-24 00:00:39 +0800

    mod: post comments css

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,11 +1,11 @@
 function show_comments_of_commentable_id(id) {
-  $("#comments_list_" + id).slideDown();
+  $("#comments_" + id).slideDown();
   $("#comments_show_link_" + id).hide();
   $("#comments_hide_link_" + id).fadeIn();
 }
 
 function hide_comments_of_commentable_id(id) {
-  $("#comments_list_" + id).slideUp();
+  $("#comments_" + id).slideUp();
   $("#comments_hide_link_" + id).hide();
   $("#comments_show_link_" + id).fadeIn();
 }
