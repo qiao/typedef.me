ef71547b639cec06cf49e4fcbec1d51df073806e
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-07-24 01:31:43 +0800

    add: gravatar for comments

--- Gemfile
+++ Gemfile
@@ -1,13 +1,13 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.9'
+gem 'rake', '0.9.2'
+gem 'sqlite3'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
-
-gem 'rake', '0.9.2'
+gem 'gravatar_image_tag'
 
 group :development do
   gem 'faker'
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,11 @@ module CommentsHelper
   def publish_time(comment)
     comment.created_at.in_time_zone(8).strftime("%Y-%m-%d %X")
   end
+
+  def gravatar_for(comment)
+    gravatar_image_tag(comment.author_email.downcase, 
+                       :alt => comment.author_name,
+                       :class => "gravatar_image",
+                       :gravatar => { :size => 32 })
+  end
 end
