735861c6eeeca100c04820f28559bfd4b5ed4264
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-08-14 21:33:45 +0800

    mod: html codes in markdowns are no longer filtered

--- app/models/commentable.rb
+++ app/models/commentable.rb
@@ -23,7 +23,7 @@ class Commentable < ActiveRecord::Base
   end
 
   def markdown(text)
-    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, 
+    options = [:hard_wrap, :autolink, :no_intraemphasis, 
                :fenced_code, :gh_blockcode]
     syntax_highlighter(Redcarpet.new(text, *options).to_html).html_safe
   end
