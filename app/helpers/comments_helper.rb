db790535049fc66b8beaf046dfb0e76ee977a14d
Xueqiao Xu <xueqiaoxu@gmail.com>
2011-08-21 21:06:02 +0800

    fix: timezone settings

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,8 +1,4 @@
 module CommentsHelper
-  def publish_time(comment)
-    comment.created_at.in_time_zone(8).strftime("%Y-%m-%d %X")
-  end
-
   def gravatar_for(comment)
     gravatar_image_tag(comment.author_email.downcase, 
                        :alt => comment.author_name,
--- app/models/commentable.rb
+++ app/models/commentable.rb
@@ -13,10 +13,6 @@ class Commentable < ActiveRecord::Base
   scope :published, where(:published => true)
   scope :draft, where(:published => false)
 
-  def published_at
-    created_at.in_time_zone(8)
-  end
-
   private
   def update_content
     self.content = markdown(self.raw_content) 
